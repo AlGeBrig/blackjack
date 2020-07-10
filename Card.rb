18d47b1254712214c09ea4f1bc6e0b9e5ae8f0d6
AlGeBrig <avenon7@gmail.com>
2020-07-11 00:00:13 +0300

    add accessor and initialize

--- Card.rb
+++ Card.rb
@@ -1,5 +1,15 @@
 class Card
 
+ SUITS = %w(+ <3 <> ^).freeze   
+ CARDS = %w(2 3 4 5 6 7 8 9 10 J Q K A).freeze
 
-    
+ attr_accessor :value, :SUITS
+
+ def initialize(value, suit)
+    @value = value
+    @suit = suit
+ end
+
+ 
+
 end
